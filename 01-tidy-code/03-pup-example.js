d69bdc0dd9e95d2c55f3765087bd9e092703d17c
Sulamita Morales <kyusulamita@users.noreply.github.com>
2019-12-30 17:13:00 -0500

    more examples
    Examples for first day and common questions

---
+++ 01-tidy-code/03-pup-example.js
@@ -0,0 +1,16 @@
+const Pup = function(name) {
+  this.name = name;
+}
+
+
+Pup.prototype.speak = function() {
+  console.log(`Woof, I am ${this.name}`);
+}
+
+
+const markPup = new Pup(`Mark`);
+
+
+// Don’t worry about what this means now!
+
+// we will cover the `this` keyword in a future lecture :)
